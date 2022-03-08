34377b1ed5be5f391b2929e6877c08eb0d5851ad
Claudia Vicol <cvi070@mmdfvffh1n5q05n.fritz.box>
2022-03-08 11:43:36 +0100

    removed unused code

--- src/main/com/helkaerea/user/service/user/User.java
+++ src/main/com/helkaerea/user/service/user/User.java
@@ -7,7 +7,7 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 /**
  * User represents someone with a name and an age.
  */
-public class User implements Comparable<User>{
+public class User {
 
     private final String name;
     private final int age;
@@ -39,18 +39,5 @@ public class User implements Comparable<User>{
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this);
-    }
-
-    public static int compareByNameThenAge(User user1, User user2) {
-        if (user1.getName().equals(user2.getName())) {
-            return user1.getAge() - (user2.getAge());
-        } else {
-            return user1.getName().compareTo(user2.getName());
-        }
-    }
-
-    @Override
-    public int compareTo(User o) {
-       return this.getName().compareTo(o.getName());
     }
 }
